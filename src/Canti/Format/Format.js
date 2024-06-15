7caae144038cc5c8eea57768751f350a52882ffd
PeterQuadrini <quadrinipietro@gmail.com>
2024-06-15 16:29:53 +0200

    newLayout2

--- src/Canti/Format/Format.js
+++ src/Canti/Format/Format.js
@@ -6,48 +6,58 @@ const Box = styled.a`
   min-height:4vh;
   width: 80%;
   max-width: 400px;
-  padding: 1%;
+  padding: 10px;
   border-bottom: solid 0.5px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-decoration: none;
   color: black;
 `;
-const Detail = styled.a`
-  width: 80%;
-  text-align: left;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
+
 const Arrow = styled.a`
-  width: 20%;
+  width: 10%;
   height: 100%;
   text-align: left;
   display: flex;
   justify-content: center;
 `;
 const Title = styled.div`
+  width: 90%;
   font-weight: 900;
   font-family: "Inter", sans-serif, "IBM Plex Mono", monospace;
   font-size: 3.2vh;
   color: #8d0000;
 `;
+const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
 const Sottotitolo = styled.div`
-  font-weight: 300;
-  ont-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-family: "Roboto", sans-serif;
+  width: 100%;
   font-size: 1.8vh;
 `;
 const Container = styled.div`
   width: 100%;
-  margin-top: 1vh;
+  margin-top: 15px;
   line-height:1.2vh;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 10px;
+  font-size: 1.5vh;
+  padding-bottom: 5px;
+  font-family: "IBM Plex Mono", monospace;
+`;
+const ContainerSection = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 10px;
   font-size: 1.5vh;
-  padding: 10px 0;
   font-family: "IBM Plex Mono", monospace;
 `;
 const ContainerItem = styled.div`
@@ -64,24 +74,29 @@ const ContainerItem = styled.div`
 
 const Item = (props) => {
   return <Box href={props.link}>
-    <Detail>
+    <TitleContainer>
       <Title>{props.title}</Title>
-      <Sottotitolo>{props.sottotitolo}</Sottotitolo>
-      <Container>
+      <Arrow>
+        <AiOutlineArrowRight />
+      </Arrow>
+    </TitleContainer>
+    <Sottotitolo>{props.sottotitolo}</Sottotitolo>
+    <Container>
+      <ContainerSection>
         { props.ingresso && <ContainerItem color='#003f5c' textColor="white">Ingresso</ContainerItem>}
         { props.pace && <ContainerItem color='#58508d' textColor="white">Pace</ContainerItem>}
         { props.pane && <ContainerItem color='#8a508f' textColor="white">Pane</ContainerItem>}
         { props.vino && <ContainerItem color='#bc5090' textColor="white">Vino</ContainerItem>}
         { props.finale && <ContainerItem color='#ffd380' textColor="black">Finale</ContainerItem>}
+      </ContainerSection>
+      <ContainerSection>
         { props.aMaria && <ContainerItem color='#de5a79' textColor="white">A Maria</ContainerItem>}
         { props.natale && <ContainerItem color='#ff6361' textColor="white">Natale</ContainerItem>}
         { props.quaresima && <ContainerItem color='#ff8531' textColor="black">Quaresima</ContainerItem>}
         { props.pentecoste && <ContainerItem color='#ffa600' textColor="black">Pentecoste</ContainerItem>}
-      </Container>
-    </Detail>
-    <Arrow>
-      <AiOutlineArrowRight />
-    </Arrow>
+      </ContainerSection>
+    </Container>
   </Box>
 }
+
 export { Item };
